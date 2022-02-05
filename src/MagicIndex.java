c4f5e6bd4a65da2fec6ab6e862d5b92c69470617
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2022-02-05 12:34:19 -0600

    find magic index for sorted array with distinct elements

---
+++ src/MagicIndex.java
@@ -0,0 +1,32 @@
+public class MagicIndex {
+
+    public boolean findMagicIndex(int[] array) {
+        return findMagicIndex(0, array.length, array);
+    }
+
+    // when elements are distinct
+    public boolean findMagicIndex(int l, int r, int[] array) {
+        if (l > r)
+            return false;
+
+        int mid = (l + r) / 2;
+
+        if (array[mid] == mid)
+            return true;
+
+        if (array[mid] < mid)
+            return findMagicIndex(mid + 1, r, array);
+        else
+            return findMagicIndex(0, mid - 1, array);
+    }
+
+    public static void main(String[] args) {
+        MagicIndex mi = new MagicIndex();
+
+        // distinct
+        System.out.println(mi.findMagicIndex(new int[] { -5, 0, 1, 2, 3, 5, 7, 10 })); // true
+        System.out.println(mi.findMagicIndex(new int[] { 0 })); // true
+        System.out.println(mi.findMagicIndex(new int[] { 1 })); // false
+        System.out.println(mi.findMagicIndex(new int[] { 1, 2, 3 })); // false
+    }
+}
